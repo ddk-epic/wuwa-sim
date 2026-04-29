aea80318291551ae96cdaaca6943a1c4fc7806b9
ddk <danhkhang95@gmail.com>
2026-04-29 23:07:19 +0200

    update generated data

--- src/data/echoes/inferno-rider.ts
+++ src/data/echoes/inferno-rider.ts
@@ -1,25 +1,25 @@
-import type { EnrichedEcho } from '#/types/echo'
+import type { EnrichedEcho } from "#/types/echo"
 
 export const infernoRider = {
   id: 390080007,
-  name: 'Inferno Rider',
+  name: "Inferno Rider",
   cost: 4,
-  element: 'Fusion',
-  set: 'Molten Rift',
+  element: "Fusion",
+  set: "Molten Rift",
   skill: {
     cooldown: 20,
     description:
       "Transform into the Inferno Rider to launch up to 3 consecutive slashes in a row, each slash dealing 242.40%, 282.80%, and 282.80% Fusion DMG respectively.After the final hit, increase the current Resonator's Fusion DMG by 12.00% and Basic Attack DMG by 12.00% for 15s.Long press the Echo Skill to transform into the Inferno Rider and enter Riding Mode. When exiting Riding Mode, deal 282.80% Fusion DMG to enemies in front.CD: 20s",
     stages: [
       {
-        name: 'Tap',
-        newName: '(Tap)',
+        name: "Tap",
+        newName: "",
         actionTime: 0,
         damage: [
           {
-            type: 'Echo Skill',
-            dmgType: 'Damage',
-            scalingStat: 'ATK',
+            type: "Echo Skill",
+            dmgType: "Damage",
+            scalingStat: "ATK",
             value: 2.424,
             energy: 3.78,
             concerto: 0,
@@ -27,9 +27,9 @@ export const infernoRider = {
             weakness: 0,
           },
           {
-            type: 'Echo Skill',
-            dmgType: 'Damage',
-            scalingStat: 'ATK',
+            type: "Echo Skill",
+            dmgType: "Damage",
+            scalingStat: "ATK",
             value: 2.828,
             energy: 4.41,
             concerto: 0,
@@ -37,9 +37,9 @@ export const infernoRider = {
             weakness: 0,
           },
           {
-            type: 'Echo Skill',
-            dmgType: 'Damage',
-            scalingStat: 'ATK',
+            type: "Echo Skill",
+            dmgType: "Damage",
+            scalingStat: "ATK",
             value: 2.828,
             energy: 4.41,
             concerto: 0,
@@ -49,15 +49,15 @@ export const infernoRider = {
         ],
       },
       {
-        name: 'Hold',
-        newName: '(Hold)',
+        name: "Hold",
+        newName: "(Hold)",
         hidden: true,
         actionTime: 0,
         damage: [
           {
-            type: 'Echo Skill',
-            dmgType: 'Damage',
-            scalingStat: 'ATK',
+            type: "Echo Skill",
+            dmgType: "Damage",
+            scalingStat: "ATK",
             value: 2.828,
             energy: 4.41,
             concerto: 0,
